3bab55bde25eff0b64e99dab933c588792ef886b
obijuan <juan@iearobotics.com>
2016-04-27 12:33:55 +0200

    Challgenche 1 done

--- 05_reto_1/main.js
+++ 05_reto_1/main.js
@@ -1,1 +1,7 @@
 fs = require('fs');
+
+fs.readFile('comida.txt', 'utf8',(err, data) => {
+  if (err) throw err;
+	console.log("Fichero leido!!");
+  console.log(data.split('\n').join("  mola\n"));
+});
